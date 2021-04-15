1b003b4e989aefc49efc5c9b6c2281ad9144fcc9
juliawiktoria <maybe.nott123@gmail.com>
2021-04-15 11:47:25 +0100

    fixes

--- compute_fid.py
+++ compute_fid.py
@@ -6,7 +6,7 @@ import pandas as pd
 import os
 import argparse
 
-def get_fid_for_dir_path(path_name, start_epoch, end_epoch, iter_step, stats_path, dataset_name, model_name, if_nvae=False):
+def get_fid_for_dir_path(path_name, start_epoch, end_epoch, iter_step, stats_path, dataset_name, model_name, if_one_dir):
     # defining stuff for FID library
     model = FrechetInceptionDistance.get_inception_model()
     model = model.cuda()
@@ -17,12 +17,14 @@ def get_fid_for_dir_path(path_name, start_epoch, end_epoch, iter_step, stats_pat
     result = []
     result_df = pd.DataFrame(columns=['epoch', 'dataset', 'FID_score'])
 
+    
+    if if_one_dir:
+        fid_score = fid.calculate_fid_given_paths(stats_path, path_name)
+        file_name = 'FID_{}_{}_{}eps.csv'.format(model_name, dataset_name, how_many_eps)
+        result_df.to_csv(file_name, index=False)
+    
     # measuring time
     start_time = time.time()
-    if if_nvae:
-        fid_score = fid.calculate_fid_given_paths(stats_path, path_name)
-        return fid_score
-
     for i in range(start_epoch, end_epoch + 1, iter_step):
         addon = 'epoch_' + str(i)
         path_to_dir = Path(os.path.join(path_name, addon))
@@ -46,6 +48,7 @@ if __name__ == "__main__":
     parser.add_argument('--stats_path', type=str, required=True, help='path to file with fid statistics for given dataset')
     parser.add_argument('--dataset_name', type=str, help='name of the used dataset')
     parser.add_argument('--model_name', type=str, help='name of the used model')
+    parser.add_argument('--one_dir', action='store_true', default=False, help="indicates computing fid for only one dir with imgs")
     args = parser.parse_args()
 
-    get_fid_for_dir_path(args.images_path, args.start_epoch, args.end_epoch, args.epoch_step, args.stats_path, args.dataset_name, args.model_name)
+    get_fid_for_dir_path(args.images_path, args.start_epoch, args.end_epoch, args.epoch_step, args.stats_path, args.dataset_name, args.model_name, args.one_dir)
